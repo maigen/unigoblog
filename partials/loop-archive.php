6e3298be1dceda94285e4607d7a2431b4b5a19dd
Maigen Thomas <mthomas@unigogroup.com>
2014-07-09 14:52:12 -0700

    changed front page and single pages to only show title in header box, archive page only showing one post

--- partials/loop-archive.php
+++ partials/loop-archive.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 						
 	<header class="article-header">
-		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"></a></h2>
 		<?php get_template_part( 'partials/content', 'byline' ); ?>
 		</p>							
 	</header> <!-- end article header -->
--- partials/loop-page.php
+++ partials/loop-page.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
 						
 	<header class="article-header">
-		<h1 class="page-title"><?php the_title(); ?></h1>
+		
 	</header> <!-- end article header -->
 					
     <section class="entry-content clearfix" itemprop="articleBody">
