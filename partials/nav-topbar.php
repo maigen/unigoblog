1c43050f27959b53a3b05159cda27b09de3cf8d1
Maigen Thomas <mthomas@unigogroup.com>
2014-07-11 12:06:21 -0700

    fixed weird issue with nav bar partially showing on scrolldown

--- partials/nav-topbar.php
+++ partials/nav-topbar.php
@@ -1,5 +1,5 @@
 <div class="large-12 columns">
-	<div class="contain-to-grid sticky">
+	<div class="contain-to-grid">
 	
 	<!-- If you want to use the more traditional "fixed" navigation.
 		 simply replace "sticky" with "fixed" -->
