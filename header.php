1abc97740224faa42749aa78c2cc23fe7391f7cd
Maigen Thomas <mthomas@unigogroup.com>
2014-07-14 08:37:59 -0700

    changed border radius on big_black_box div

--- header.php
+++ header.php
@@ -119,9 +119,7 @@
                                             $author_id = $post->post_author;
                                         ?>
                                             <h1>
-
                                                 <span><?php _e("Posts By:", "jointstheme"); ?></span> <?php echo get_the_author_meta('display_name', $author_id); ?>
-
                                             </h1>
                                         <?php } elseif (is_day()) { ?>
                                             <h1>
