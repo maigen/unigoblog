3e7d4e57f4e389d50d0c9b584361de0d5259b4a9
unknown <dadams@W7-DADAMS.acm.local>
2014-07-16 17:05:14 -0700

    working carousel with image

--- header.php
+++ header.php
@@ -109,27 +109,27 @@
                              </div>
 
 
-                             <div class="small-12 columns">
-                             <ul class="example-orbit" data-orbit>
-                               <li>
-                                 <img src="uci-aldrich.jpg" alt="slide 1" />
-                                 <div class="orbit-caption">
-                                   Caption One.
-                                 </div>
-                               </li>
-                               <li class="active">
-                                 <img src="../assets/img/examples/andromeda-orbit.jpg" alt="slide 2" />
-                                 <div class="orbit-caption">
-                                   Caption Two.
-                                 </div>
-                               </li>
-                               <li>
-                                 <img src="../assets/img/examples/launch-orbit.jpg" alt="slide 3" />
-                                 <div class="orbit-caption">
-                                   Caption Three.
-                                 </div>
-                               </li>
-                             </ul>
+                            <div class="small-12 columns">
+                              <ul class="example-orbit" data-orbit>
+                                <li>
+                                  <img src="uci-aldrich.jpg" alt="slide 1" />
+                                  <div class="orbit-caption">
+                                    Caption One.
+                                  </div>
+                                </li>
+                                <li class="active">
+                                  <img src="uci-aldrich.jpg" alt="slide 1" />
+                                  <div class="orbit-caption">
+                                    Caption Two.
+                                  </div>
+                                </li>
+                                <li>
+                                  <img src="uci-aldrich.jpg" alt="slide 1" />
+                                  <div class="orbit-caption">
+                                    Caption Three.
+                                  </div>
+                                </li>
+                              </ul>
                             </div>
 
                              <!-- <div class="large-12 columns medium-12 columns" id="big_black_box">
