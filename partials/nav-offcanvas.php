b2bc4f16e341cfe18d0e03e2a07d4db9a37d7ddc
unknown <dadams@W7-DADAMS.acm.local>
2014-07-17 16:54:32 -0700

    styles hamburger and makes offcanvas menu to the right

--- partials/nav-offcanvas.php
+++ partials/nav-offcanvas.php
@@ -1,37 +1,17 @@
-<div class="large-12 columns show-for-medium-up" id="navigation_bar">
-
-		<!-- If you want to use the more traditional "fixed" navigation.
-		 simply replace "sticky" with "fixed" -->
-
-		<nav class="top-bar" data-topbar>
-			<ul class="title-area">
-				<!-- Title Area -->
-
-				<li class="toggle-topbar menu-icon">
-					<a href="#"><span>Menu</span></a>
-				</li>
-			</ul>		
-			<section class="top-bar-section">
-				<?php joints_main_nav(); ?>
-			</section>
-		</nav>
-
-</div>
-
 <div class="large-12 columns show-for-small-only">
 	<div class="contain-to-grid">
 		<nav class="tab-bar">
 			<section class="middle tab-bar-section">
 				<!-- <h1 class="title"><?php bloginfo('name'); ?></h1> -->
 			</section>
-			<section class="left-small">
-				<a class="left-off-canvas-toggle menu-icon" ><span></span></a>
+			<section class="right-small">
+				<a class="right-off-canvas-toggle menu-icon" ><span></span></a>
 			</section>
 		</nav>
 	</div>
 </div>
 						
-<aside class="left-off-canvas-menu show-for-small-only">
+<aside class="right-off-canvas-menu show-for-small-only">
 	<ul class="off-canvas-list">
 		<li><label>Unigo</label></li>
 			<?php joints_main_nav(); ?> 
