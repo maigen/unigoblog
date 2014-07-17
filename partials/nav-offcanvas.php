89aacc1cddeee639edf8f5a7a8403b2823bcdc74
Maigen Thomas <mthomas@unigogroup.com>
2014-07-16 18:28:19 -0700

    manually ported over davids changes from the top_nav_styling branch

--- partials/nav-offcanvas.php
+++ partials/nav-offcanvas.php
@@ -1,9 +1,9 @@
 <div class="large-12 columns show-for-medium-up" id="navigation_bar">
-	<div class="sticky">
-	
+
+
 		<!-- If you want to use the more traditional "fixed" navigation.
 		 simply replace "sticky" with "fixed" -->
-	
+
 		<nav class="top-bar" data-topbar>
 			<ul class="title-area">
 				<!-- Title Area -->
@@ -16,14 +16,14 @@
 				<?php joints_main_nav(); ?>
 			</section>
 		</nav>
-	</div>
+
 </div>
 
 <div class="large-12 columns show-for-small-only">
 	<div class="contain-to-grid">
 		<nav class="tab-bar">
 			<section class="middle tab-bar-section">
-				<h1 class="title"><?php bloginfo('name'); ?></h1>
+				<!-- <h1 class="title"><?php bloginfo('name'); ?></h1> -->
 			</section>
 			<section class="left-small">
 				<a class="left-off-canvas-toggle menu-icon" ><span></span></a>
@@ -34,8 +34,11 @@
 						
 <aside class="left-off-canvas-menu show-for-small-only">
 	<ul class="off-canvas-list">
-		<li><label>Navigation</label></li>
-			<?php joints_main_nav(); ?>    
+		<li><label>Unigo</label></li>
+			<?php joints_main_nav(); ?> 
+		<li><a href="#">Become a Member</a></li>
+		<li><a href="#">Sign In</a></li>
+
 	</ul>
 </aside>
 			
--- partials/nav-topbar.php
+++ partials/nav-topbar.php
@@ -1,81 +1,80 @@
-
-		<nav id="cbp-hrmenu" class="cbp-hrmenu">
-		            <ul>
-		                <li><a href="#">College Match</a>
-		                    <div class="cbp-hrsub">
-		                        <div class="cbp-hrsub-inner"> 
-		                            <div>
-		                            <ul>
-		                              <li><a href="<% =URLHelper.CollegeMatch%>">College Match</a></li>
-		                              <li><a href="/Featured/UnigoCollegeRankings.aspx">College Rankings</a></li>
-		                              <li><a href="/careers/default.aspx">Career Search</a></li>
-		                              <li><a href="/methodology">MyROE&#153;</a></li>
-		                            </ul>
-		                            </div>
-		                        </div>
-		                      </div>
-		                </li>
-		                <li><a href="#">College Search</a>
-		                    <div class="cbp-hrsub">
-		                        <div class="cbp-hrsub-inner"> 
-		                            <div>
-		                            <ul>
-		                              <li><a href="/Colleges">College Search</a></li>
-		                              <li><a href="/search/FindACollege.aspx">Advanced Search</a></li>
-		                              <li><a href="/Search/StateSearch.aspx">Search by State</a></li>
-		                              <li><a href="/search/majorSearch.aspx">Search by Major</a></li>
-		                            </ul>
-		                            </div>
-		                        </div>
-		                      </div>
-		                </li>
-		                <li><a href="#">Admissions Advice</a>
-		                    <div class="cbp-hrsub">
-		                        <div class="cbp-hrsub-inner"> 
-		                            <div>
-		                            <ul>
-		                              <li><a href="/AskAnExpert.aspx">Expert Admissions Advice</a></li>
-		                              <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=4">The College Search</a></li>
-		                              <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=3">The Application Process</a></li>
-		                              <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=1">Admissions Decisions</a></li>
-		                            </ul>
-		                            </div>
-		                            <div>
-		                            <ul>
-		                              <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=5">Financial Aid</a></li>
-		                              <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=2">Succeeding in College</a></li>
-		                            </ul>
-		                            </div>
-		                        </div>
-		                      </div>
-		                </li>
-		                <li><a href="#">Live Counseling</a>
-		                    <div class="cbp-hrsub">
-		                        <div class="cbp-hrsub-inner"> 
-		                            <div>
-		                            <ul>
-		                              <li><a href="/Chat/Chats.aspx">Admissions Experts</a></li>
-		                              <li><a href="/search/peoplefinder.aspx?findertype=counselor">Search Counselors</a></li>
-		                              <li><a href="/askanexpert.aspx">Admissions Advice</a></li>
-		                              <li><a href="/becomeamembercompare.aspx">Compare Plans</a></li>
-		                            </ul>
-		                            </div>
-		                        </div>
-		                      </div>
-		                </li>
-		                <li><a href="#">Absolute Admit&#153;</a>
-		                    <div class="cbp-hrsub">
-		                        <div class="cbp-hrsub-inner"> 
-		                            <div>
-		                            <ul>
-		                              <li><a href="/Curriculum">Absolute Admit&#153;</a></li>
-		                              <li><a href="/becomeamembercompare.aspx">Compare Plans</a></li>
-		                              <li><a href="/Chat/AccessForAll.aspx">Access For All</a></li>
-		                              <li><a href="/becomeamembercompare.aspx">Become a Member</a></li>
-		                            </ul>
-		                            </div>
-		                        </div>
-		                      </div>
-		                </li>           
-		            </ul>
-		        </nav>
+<nav id="cbp-hrmenu" class="cbp-hrmenu hide-for-small">
+    <ul>
+        <li><a href="#">College Match</a>
+            <div class="cbp-hrsub">
+                <div class="cbp-hrsub-inner"> 
+                    <div>
+                    <ul>
+                      <li><a href="<% =URLHelper.CollegeMatch%>">College Match</a></li>
+                      <li><a href="/Featured/UnigoCollegeRankings.aspx">College Rankings</a></li>
+                      <li><a href="/careers/default.aspx">Career Search</a></li>
+                      <li><a href="/methodology">MyROE&#153;</a></li>
+                    </ul>
+                    </div>
+                </div>
+              </div>
+        </li>
+        <li><a href="#">College Search</a>
+            <div class="cbp-hrsub">
+                <div class="cbp-hrsub-inner dd2"> 
+                    <div>
+                    <ul>
+                      <li><a href="/Colleges">College Search</a></li>
+                      <li><a href="/search/FindACollege.aspx">Advanced Search</a></li>
+                      <li><a href="/Search/StateSearch.aspx">Search by State</a></li>
+                      <li><a href="/search/majorSearch.aspx">Search by Major</a></li>
+                    </ul>
+                    </div>
+                </div>
+              </div>
+        </li>
+        <li><a href="#">Admissions Advice</a>
+            <div class="cbp-hrsub">
+                <div class="cbp-hrsub-inner dd3"> 
+                    <div>
+                    <ul>
+                      <li><a href="/AskAnExpert.aspx">Expert Admissions Advice</a></li>
+                      <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=4">The College Search</a></li>
+                      <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=3">The Application Process</a></li>
+                      <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=1">Admissions Decisions</a></li>
+                    </ul>
+                    </div>
+                    <div>
+                    <ul>
+                      <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=5">Financial Aid</a></li>
+                      <li><a href="/AdmissionsAdvice//Default.aspx?bucketId=2">Succeeding in College</a></li>
+                    </ul>
+                    </div>
+                </div>
+              </div>
+        </li>
+        <li><a href="#">Live Counseling</a>
+            <div class="cbp-hrsub">
+                <div class="cbp-hrsub-inner dd4"> 
+                    <div>
+                    <ul>
+                      <li><a href="/Chat/Chats.aspx">Admissions Experts</a></li>
+                      <li><a href="/search/peoplefinder.aspx?findertype=counselor">Search Counselors</a></li>
+                      <li><a href="/askanexpert.aspx">Admissions Advice</a></li>
+                      <li><a href="/becomeamembercompare.aspx">Compare Plans</a></li>
+                    </ul>
+                    </div>
+                </div>
+              </div>
+        </li>
+        <li><a href="#">Absolute Admit&#153;</a>
+            <div class="cbp-hrsub">
+                <div class="cbp-hrsub-inner dd5"> 
+                    <div>
+                    <ul>
+                      <li><a href="/Curriculum">Absolute Admit&#153;</a></li>
+                      <li><a href="/becomeamembercompare.aspx">Compare Plans</a></li>
+                      <li><a href="/Chat/AccessForAll.aspx">Access For All</a></li>
+                      <li><a href="/becomeamembercompare.aspx">Become a Member</a></li>
+                    </ul>
+                    </div>
+                </div>
+              </div>
+        </li>           
+    </ul>
+</nav>
